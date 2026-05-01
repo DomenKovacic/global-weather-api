954b7d0bd5b5907442890dd33ed53517ab7e30bf
Domen <domen.kovacic01@gmail.com>
2026-05-01 17:39:25 +0200

    Spring boot project setup

---
+++ src/main/java/com/domen/weather/GlobalWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.domen.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalWeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalWeatherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domen/weather/GlobalWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domen.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalWeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
